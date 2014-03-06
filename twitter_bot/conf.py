a5b0cb9f79c71139a52a4bda392477b5da1e43c1
nks.sai <nkssai@outlook.com>
2014-03-06 19:20:22 +0800

    修改 conf,删除user部分

--- twitter_bot/conf.py
+++ twitter_bot/conf.py
@@ -3,14 +3,11 @@ u"""
 twitter_bot conf.py
 nks.sai
 2013-3-4
-配置运行环境以及twitter用户部分
+配置运行环境
 """
 
 env = 'local'
 proxy = '127.0.0.1:8087'
-
-username = ''
-passwd = ''
 
 token_url = 'https://twitter.com/login'
 login_url = 'https://twitter.com/sessions'
